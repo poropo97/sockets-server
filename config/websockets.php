ff13b1169669328ea320b18e4d08bf980612cdb9
poropo97 <@Ganimedes97@>
2022-09-15 17:09:49 +0200

    new

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/opt/psa/var/certificates/scfzTpHUm'),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/opt/psa/var/certificates/scfUEzYSC'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/opt/psa/var/certificates/scfzTpHUm'),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/opt/psa/var/certificates/scfUEzYSC'),
 
         /*
          * Passphrase for your local_cert file.
